cfcb4a080a76f8ccedef926235b002a1b3ce4cd7
smccooey <smccooey@oswego.edu>
2016-04-14 11:02:53 -0400

    Test

--- TexasHoldem/src/csc445a3texasholdem/Deck.java
+++
@@ -1,7 +0,0 @@
-package csc445a3texasholdem;
-
-/**
- * Created by Sean on 4/14/2016.
- */
-public class Deck {
-}
---
+++ TexasHoldem/src/texasholdem/Main.java
@@ -0,0 +1,11 @@
+package texasholdem;
+
+/**
+ * Main class
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
