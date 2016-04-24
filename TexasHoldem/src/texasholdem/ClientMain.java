03526005ebb6ba449106b3bced7dc08d124bf165
smccooey <smccooey@oswego.edu>
2016-04-23 21:29:21 -0400

    Separate main classes for client and server.

---
+++ TexasHoldem/src/texasholdem/ClientMain.java
@@ -0,0 +1,13 @@
+package texasholdem;
+
+import texasholdem.client.GameClient;
+
+/**
+ * Main class for client
+ */
+public class ClientMain {
+
+    public static void main(String[] args) {
+        new GameClient();
+    }
+}
---
+++ TexasHoldem/src/texasholdem/ServerMain.java
@@ -0,0 +1,13 @@
+package texasholdem;
+
+import texasholdem.server.GameServer;
+
+/**
+ * Main class for server
+ */
+public class ServerMain {
+
+    public static void main(String[] args) {
+        new GameServer();
+    }
+}
