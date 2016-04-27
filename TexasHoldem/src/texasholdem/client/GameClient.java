a7780a11fd74b45f4ad848a64d0aa70a191eee92
smccooey <smccooey@users.noreply.github.com>
2016-04-27 12:05:49 -0400

    Update GameClient.java

--- TexasHoldem/src/texasholdem/client/GameClient.java
+++ TexasHoldem/src/texasholdem/client/GameClient.java
@@ -1,12 +1,16 @@
 package texasholdem.client;
 
+import texasholdem.SharedUtilities;
 import texasholdem.TexasHoldemConstants;
 import texasholdem.gamestate.GameState;
 import texasholdem.gamestate.Player;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.SocketException;
+import java.util.Scanner;
 
 /**
  * GameClient. Includes a separate listener thread to receive incoming
@@ -45,16 +49,39 @@ public class GameClient implements TexasHoldemConstants {
    private Player player;
 
    /**
+    * Reads user input
+    */
+   private Scanner in;
+
+   /**
+    * Timeout before the client assumes that the server is no longer connected
+    */
+   private final static int DROP_TIMEOUT = 5 * HEARTBEAT_INTERVAL;
+
+   /**
+    * true iff the player is the OP and is in the pregame mode
+    */
+   private volatile boolean pregame;
+
+   /**
+    * The player's name
+    */
+   private String name;
+
+   /**
     * Constructs a client in the Texas Hold 'em game.
     */
    public GameClient() {
+      in = new Scanner(System.in);
+      System.out.println("Enter your name: ");
+      name = in.nextLine();
+      pregame = false;
       try {
          group = InetAddress.getByName(MULTICAST_ADDRESS);
          server = InetAddress.getByName(SERVER_ADDRESS);
          socket = new MulticastSocket(PORT);
          socket.joinGroup(group);
-         player = new Player(socket.getNetworkInterface()
-               .getHardwareAddress());
+         socket.setSoTimeout(DROP_TIMEOUT);
       }
       catch(IOException ioe) {
          ioe.printStackTrace();
@@ -75,6 +102,29 @@ public class GameClient implements TexasHoldemConstants {
       if(obj == null) {
          throw new NullPointerException("Null received.");
       }
+      else if(obj instanceof Player) {
+         // This should only occur when the player first joins the game
+         player = (Player)obj;
+         player.setName(name);
+         try {
+            player.setId(socket.getNetworkInterface().getHardwareAddress());
+         }
+         catch(SocketException se) {
+            se.printStackTrace();
+            System.out.println("Socket error.");
+            System.exit(1);
+         }
+         // Return the player to the server
+         try {
+            byte[] playerBytes = SharedUtilities.toByteArray(player);
+            DatagramPacket packet = new DatagramPacket(playerBytes,
+                  playerBytes.length, server, PORT);
+            socket.send(packet);
+         }
+         catch(IOException ioe) {
+            ioe.printStackTrace();
+         }
+      }
       else if(obj instanceof GameState) {
          // Do stuff
       }
@@ -84,8 +134,8 @@ public class GameClient implements TexasHoldemConstants {
     * Returns the player's unique id.
     * @return The player's id
     */
-   public byte[] id() {
-      return player.id();
+   public byte[] getId() {
+      return player.getId();
    }
 
    /**
